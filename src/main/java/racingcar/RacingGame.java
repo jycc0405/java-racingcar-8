a139c0ca0f33c79edbc62ef575152c31b3523890
JY <jycc0405@naver.com>
2025-10-21 17:03:51 +0900

    기능(Application, RacingGame) : 구현할 기능1 추가
    구현할 기능 1 구현 완료
    (README 참조)

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,57 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGame {
+
+    private List<Car> carList = new ArrayList<>();
+    private int round = 0;
+
+
+    public void start() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNamesInput = Console.readLine();
+
+        if (carNamesInput == null) {
+            throw new IllegalArgumentException("입력이 비어있습니다.");
+        }
+
+        String[] carNames = carNamesInput.split(",");
+        for (int i = 0; i < carNames.length; i++) {
+            carNames[i] = carNames[i].trim();
+            if (carNames[i].isEmpty() || carNames[i].length() > 5) {
+                throw new IllegalArgumentException("자동차 이름이 비었거나 5자를 초과했습니다.");
+            }
+            carList.add(new Car(carNames[i]));
+        }
+
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String roundInput = Console.readLine();
+
+        if (roundInput == null) {
+            throw new IllegalArgumentException("입력이 비어있습니다.");
+        }
+
+        try {
+            round = Integer.parseInt(roundInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자 형식이 아닙니다");
+        }
+    }
+
+    public static class Car {
+        private String name;
+
+        public Car(String name) {
+            this.name = name;
+        }
+
+        public String getName(){
+            return name;
+        }
+    }
+}
